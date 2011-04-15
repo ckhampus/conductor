bdc45eacdadc104c4585826c9c58c7749a375127
Cristian Hampus <ckhampus@gmail.com>
2011-04-15 14:22:45 +0000

    Split some tests in the route test.

--- tests/Conductor/RouteTest.php
+++ tests/Conductor/RouteTest.php
@@ -5,31 +5,47 @@ use Conductor\Route;
 class RouteTest extends PHPUnit_Framework_TestCase {
     public function testRoute()
     {
+        // Create a new route
         $r1 = new Route('GET', '/hello', function() {});
         
+        // Get the route method and path
         $this->assertEquals('GET', $r1->getMethod());
         $this->assertEquals('/hello', $r1->getPath());
         
+        // Get the route name before assignment
+        $this->assertEquals(NULL, $r1->getName());
+        
+        // Give the route a name
         $r1->setName('hello');
         
+        // Get the route name after assignment
         $this->assertEquals('hello', $r1->getName());
+    }
+    
+    public function testRouteWithData() {
+        // Create a new route with parameter in path
+        $r1 = new Route('GET', '/hello/:name', function($name) {});
 
-        $r2 = new Route('GET', '/hello/:name', function($name) {});
-
-        $this->assertEquals('/hello/:name', $r2->getPath());
-        $this->assertEquals('/hello/cristian', $r2->getPath(array('cristian')));
+        // Get the path with and without parameters data
+        $this->assertEquals('/hello/:name', $r1->getPath());
+        $this->assertEquals('/hello/cristian', $r1->getPath(array('cristian')));
     }
 
-    public function testRouteMatching()
-    {
+    public function testRouteMatching() {
+        // Create a new route
         $r1 = new Route('GET', '/hello', function() {});
 
+        // Match the route against a valid and invalid path
         $this->assertTrue($r1->matchRoute('/hello'));
         $this->assertFalse($r1->matchRoute('/world'));
-        
-        $r2 = new Route('GET', '/hello/:name', function($name) {});
+    }
+    
+    public function testRouteMatchingWithData() {
+        // Create a new route with parameter in path
+        $r1 = new Route('GET', '/hello/:name', function($name) {});
 
-        $this->assertTrue($r2->matchRoute('/hello/cristian'));
-        $this->assertFalse($r2->matchRoute('/gello/cristian'));
+        // Match the route against a valid and invalid path
+        $this->assertTrue($r1->matchRoute('/hello/cristian'));
+        $this->assertFalse($r1->matchRoute('/gello/cristian'));
     }
 }

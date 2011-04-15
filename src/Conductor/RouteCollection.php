d65bdd3c9dbee95cfde17bff634d89fa8304b798
Cristian Hampus <ckhampus@gmail.com>
2011-04-15 13:32:08 +0000

    More work on the RouteCollection.

--- src/Conductor/RouteCollection.php
+++ src/Conductor/RouteCollection.php
@@ -3,6 +3,8 @@
 namespace Conductor;
 
 use Conductor\Base;
+use Conductor\Route;
+
 /**
  * Collection object for routes.
  * 
@@ -10,7 +12,22 @@ use Conductor\Base;
  */
 class RouteCollection extends Base implements Iterator, Countable {
     private $position = 0;
+    private $count = 0;
     private $routes = array();
+    
+    public function add(Route $route) {
+        $this->routes = $route;
+        $this->count = count($this->routes);
+    }
+    
+    /**
+     * Count the number of routes in the collection.
+     *
+     * @return int
+     */
+    public function count() {
+        return $this->count;   
+    }
     
     public function current() {
         return $this->routes[$this->position];

d92bd657dc014ef049e00ff806f1fc42a59ce6c4
Cristian Hampus <contact@cristianhampus.se>
2011-04-17 01:43:57 +0200

    Created new branch for request class.

---
+++ src/Conductor/Http/Request.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Conductor\Http;
+
+/**
+ * 
+ **/
+class Request
+{
+    
+    function __construct()
+    {
+        
+    }
+
+    public function addCookies(array $cookies)
+    {
+        // code...  
+    }
+
+    public function addHeaders(array $headers)
+    {
+        // code...  
+    }
+}
---
+++ tests/Conductor/Http/RequestTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Conductor\Http\Request;
+
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+     public function testAddingCookie()
+     {
+         $r = new Request();
+
+         $c1 = array('hello' => 'world');
+
+         $this->assertTrue($r->addCookies($c1));
+
+         $c2 = array('random_value');
+
+         $this->assertFalse($r->addCookies($c2));
+     }
+     
+     public function testAddingHeaders()
+     {
+         $r = new Request();
+
+         $h1 = array('Content-Type' => 'text/html');
+
+         $this->assertTrue($r->addHeaders($h1));
+
+         $h2 = array('random_value');
+
+         $this->assertFalse($r->addHeaders($h2));
+     }
+
+}

b668d174b7b9992da7333e7a7cc3bf226f12d408
Cristian Hampus <ckhampus@gmail.com>
2011-04-17 13:35:34 +0000

    Removed Autoloader class.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,18 @@
 <?php
 
-require_once(__DIR__.'/../src/Conductor/Autoloader.php');
-Conductor\Autoloader::register();
+spl_autoload_register(function($class) {
+    // Check if class or interface has been loaded allready
+    if (class_exists($class, false) || interface_exists($class, false)) {
+        return true;
+    }
+    
+    $file = sprintf('%s/%s.php', realpath(__DIR__.'/..'), str_replace('\\', '/', $class));
+    
+    // Include file if it exists
+    if (file_exists($file)) {
+        include($file);
+        return true;
+    }
+
+    return false; 
+});
